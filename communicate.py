ff2b6bdc7a88de0c50caea0649d26ae0b9518c46
vinnnyr <vruia@me.com>
2018-07-20 16:58:29 -0400

    Removed graphing funcitonality, modualrized communciation

---
+++ communicate.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import socket
+from umodbus import conf
+from umodbus.client import tcp
+
+# Enable values to be signed (default is False).
+conf.SIGNED_VALUES = True
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def connect(ip):
+    #ip = '10.0.2.137' #ip of the WTS
+    print(ip)
+    print("Attempting socket connection to " + ip)
+    sock.connect((ip, 502))
+    if sock:
+        print("Success!")
+
+def send_message():
+    message = tcp.read_holding_registers(slave_id = 1, starting_address = 0, quantity = 4)
+    try:
+        response = tcp.send_message(message, sock)
+        return response
+    except ValueError:
+        print("Whoops... WTS seems to be not responding")
+
+def try_reset():
+    print("Trying Reset")
+    message = tcp.write_single_register(1, 253 , value = 1)
+    print(message)
+    try:
+        response = tcp.send_message(message, sock)
+        return response
+    except ValueError:
+        print("Reset didn't work")
+
+def close_sock():
+    sock.close()
+    print(" \n Socket successfully closed")
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import communicate
+import time
+
+xs = []
+ys = []
+i = 0
+communicate.connect(ip = '10.0.2.137')
+scaleValue = 16 #This is the temperature scale value we believe will work
+try:
+    while True:
+        time.sleep(0.5)
+        response = communicate.send_message()
+        try:
+            temps = [(float(t)/scaleValue) for t in response]
+            print(temps)
+            xs.append(i)
+            ys.append(temps[0])
+        except:
+            communicate.try_reset()
+            print("No temperature reported")
+        i += 1
+
+
+except KeyboardInterrupt:
+    communicate.close_sock()
+    pass
+
+
+
