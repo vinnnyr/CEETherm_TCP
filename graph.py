9f876f0def547b36996b6ad59425cbc814aff956
vinnnyr <vruia@me.com>
2018-07-23 16:50:50 -0400

    Tkinter Matplotlib backend  doesnt work on mac... committed to try on PC

---
+++ graph.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import Tkinter as tk
+
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+
+LARGE_FONT= ("Verdana", 12)
+
+class App(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        
+        tk.Tk.__init__(self, *args, **kwargs)
+        
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        frame = StartPage(container, self)
+        self.frames[StartPage] = frame
+        frame.grid(row=0, column=0, sticky="nsew")
+        
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+class StartPage(tk.Frame):
+     
+     def __init__(self,parent,controller):
+        
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+mainApp = App()
+mainApp.mainloop()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-import communicate
 import time
+import communicate
 
 xs = []
 ys = []
