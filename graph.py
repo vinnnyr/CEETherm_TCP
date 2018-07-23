1843f74780c75b093fa3d4988c7eae5daad59453
Ruia <vruia3@gatech.edu>
2018-07-23 17:21:38 -0400

    Some GUI stuff

--- graph.py
+++ graph.py
@@ -8,6 +8,9 @@ from matplotlib.figure import Figure
 
 LARGE_FONT= ("Verdana", 12)
 
+def qf(quickPrint):
+    print(quickPrint)
+
 class App(tk.Tk):
 
     def __init__(self, *args, **kwargs):
@@ -20,10 +23,10 @@ class App(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        frame = StartPage(container, self)
-        self.frames[StartPage] = frame
-        frame.grid(row=0, column=0, sticky="nsew")
-        
+        for F in (StartPage,GraphPage):
+            frame = F(container,self)
+            self.frames[F] = frame
+            frame.grid(row=0,column=0,sticky="nsew")
         self.show_frame(StartPage)
 
     def show_frame(self, cont):
@@ -38,5 +41,29 @@ class StartPage(tk.Frame):
         label = tk.Label(self, text="Start Page", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
 
+        button = tk.Button(self, text="Visit Page 1", command = lambda : qf("Passing vars"))
+        button.pack()
+
+class GraphPage(tk.Frame):
+
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self,text="Graph Page!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = tk.Button(self,text="Back to Home", command = lambda : controller.show_frame(StartPage))
+        button1.pack()
+
+        f = Figure(figsize= (5,5), dpi=100)
+        a = f.add_subplot(111)
+        a.plot([1,2,3,4,5],[10,2,3,6,4])
+
+        canvas = FigureCanvasTkAgg(f,self)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand = True)
+
+        toolbar = NavigationToolbar2TkAgg(canvas,self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand = True)
 mainApp = App()
 mainApp.mainloop()
